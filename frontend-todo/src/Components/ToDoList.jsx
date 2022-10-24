a5f7312ea64ae68167b0796a1bbc969d1c6b84c5
skydodle <skydodle@gmail.com>
2022-10-24 11:04:14 +0200

    Hide delete button if 0 task

--- frontend-todo/src/Components/ToDoList.jsx
+++ frontend-todo/src/Components/ToDoList.jsx
@@ -3,22 +3,27 @@ import ToDoItem from './ToDoItem';
 
 const ToDoList = ({ toDoList, handleToggle, handleDelete, handleUpdate }) => {
   return (
-    <ul className="todo-list stack-large stack exception" role="list">
-      {toDoList.map((item) => {
-        return (
-          <ToDoItem
-            todo={item}
-            key={item.id}
-            handleToggle={handleToggle}
-            handleUpdate={handleUpdate}
-          />
-        );
-      })}
-
-      <button type="button" className="btn btn__danger" onClick={handleDelete}>
-        Delete Completed
-      </button>
-    </ul>
+    <div>
+      <ul className="todo-list stack-large stack exception" role="list">
+        {toDoList.map((item) => {
+          return (
+            <ToDoItem
+              todo={item}
+              key={item.id}
+              handleToggle={handleToggle}
+              handleUpdate={handleUpdate}
+            />
+          );
+        })}
+      </ul>
+      {toDoList.length ? (
+        <button type="button" className="btn btn__danger" onClick={handleDelete}>
+          Delete Completed
+        </button>
+      ) : (
+        ''
+      )}
+    </div>
   );
 };
 
