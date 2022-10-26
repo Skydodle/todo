ec40389cedb51d5eee4fd118b272b29b76f63d52
skydodle <skydodle@gmail.com>
2022-10-26 12:59:38 +0200

    Simplified useLocalStorage hook for browser only (took out window);code cleanup comments

--- frontend-todo/src/utils/useLocalStorage.js
+++ frontend-todo/src/utils/useLocalStorage.js
@@ -4,36 +4,17 @@ function useLocalStorage(key, initialValue) {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
-    if (typeof window === 'undefined') {
-      return initialValue;
-    }
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key);
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      // If error also return initialValue
-      console.log(error);
-      return initialValue;
-    }
+    const savedList = localStorage.getItem(key);
+    return savedList ? JSON.parse(savedList) : initialValue;
   });
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
+
+  // Define a setValue function to redine the useState hook to set localStorage
+  // Allow value to be a function so we have same API as useState
+  // Every time save state also save to localStorage
   const setValue = (value) => {
-    try {
-      // Allow value to be a function so we have same API as useState
-      const valueToStore = value instanceof Function ? value(storedValue) : value;
-      // Save state
-      setStoredValue(valueToStore);
-      // Save to local storage
-      if (typeof window !== 'undefined') {
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
-      }
-    } catch (error) {
-      // A more advanced implementation would handle the error case
-      console.log(error);
-    }
+    const valueToStore = value instanceof Function ? value(storedValue) : value;
+    setStoredValue(valueToStore);
+    localStorage.setItem(key, JSON.stringify(valueToStore));
   };
   return [storedValue, setValue];
 }
